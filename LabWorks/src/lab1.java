d02ac3aac16d6a16551397b6c825821c9e7141c3
rose <rose.dangol23@gmail.com>
2025-06-05 16:03:18 +0545

    .

--- LabWorks/src/lab1.java
+++ LabWorks/src/lab1.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class lab1 {
     public static void main(String[] args) {
         System.out.print("Rose Dangol (28684/078) \n");
         System.out.print("Lab no.1 \n\n");
